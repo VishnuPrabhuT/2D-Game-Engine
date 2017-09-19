e513de60770c55f0704a189a25962655a04083cb
Vishnuprabhut <vishnuprabhut@gmail.com>
2017-09-19 01:33:39 -0400

    Cat drawn

--- vthirug_Projects/Project 2/main.cpp
+++ vthirug_Projects/Project 2/main.cpp
@@ -3,42 +3,62 @@
 
 const int WINDOW_WIDTH=750;
 const int WINDOW_HEIGHT=500;
-const int EYE_RADIUS=32;
-
-void drawSaiyin(SDL_Renderer* renderer,SDL_Point center,SDL_Color color){
-	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+class BlackCat{
+public:
+	BlackCat(){
+				
+	}	
+	BlackCat(SDL_Renderer* rend,SDL_Point cen,SDL_Color col):renderer(rend),center(cen),color(col){
+			
+	}
+	void drawSaiyin(){
+		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+		SDL_Rect body = {center.x+15, center.y-0, 150, 175};
+	  	SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+	  	SDL_RenderFillRect( renderer, &body );
 	
-	//SDL_RenderDrawLine(renderer, center.x+65, center.y+0, center.x+50, center.y+75);
-	//SDL_RenderDrawLine(renderer, center.x-65, center.y+0, center.x-50, center.y+75);
-	//SDL_RenderDrawLine(renderer, center.x-65, center.y+0, center.x-50, center.y+75);
+		SDL_Rect head = {center.x-35, center.y-200, 250, 200};
+	  	SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+	  	SDL_RenderFillRect( renderer, &head );
 
-	//SDL_RenderDrawLine(renderer, center.x+50, center.y+75, center.x+0, center.y+100);
-	//SDL_RenderDrawLine(renderer, center.x-50, center.y+75, center.x+0, center.y+100);
-	//SDL_RenderDrawLine(renderer, 300, 240, 340, 240);
-  	//SDL_RenderDrawLine(renderer, 340, 240, 320, 200);
-	SDL_Rect body = {center.x+15, center.y-0, 150, 175};
-  	SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
-  	SDL_RenderFillRect( renderer, &body );
+		SDL_Rect mouth = {center.x+15, center.y-75, 150, 60};
+	  	SDL_SetRenderDrawColor( renderer, 255, 80, 80, 255/2 );
+	  	SDL_RenderFillRect( renderer, &mouth );
+
+		SDL_Point v1={center.x+24,center.y+25};
+		SDL_Point v2={center.x+51,center.y+25};
+		SDL_Point v3={center.x+14,center.y+10};	
+		SDL_SetRenderDrawColor(renderer,255,255,255,255);
+		SDL_RenderDrawLine(renderer, v1.x, v1.y, v2.x, v2.y);
+		SDL_RenderDrawLine(renderer, v2.x, v2.y, v3.x, v3.y);
+		SDL_RenderDrawLine(renderer, v3.x, v3.y, v1.x, v1.y);
 	
-	SDL_Rect head = {center.x-10, center.y-200, 200, 200};
-  	SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
-  	SDL_RenderFillRect( renderer, &head );
-	
-	SDL_SetRenderDrawColor(renderer,254,254,20,255);
-	for (int w = 0; w < EYE_RADIUS * 2; w++) {
-    		for (int h = 0; h < EYE_RADIUS * 2; h++) {
-      			int dx = EYE_RADIUS - w;
-      			int dy = EYE_RADIUS - h;
-      			if ((dx*dx + dy*dy) <= (EYE_RADIUS * EYE_RADIUS)) {
-        			SDL_RenderDrawPoint(renderer, center.x + dx+37, center.y + dy-140);
-				SDL_RenderDrawPoint(renderer, center.x + dx+137, center.y + dy-140);
-      			}
-    		}
-  	}
+		//SDL_SetRenderDrawColor(renderer,254,254,20,255);
+		for (int w = 0; w < EYE_RADIUS * 2; w++) {
+	    		for (int h = 0; h < EYE_RADIUS * 2; h++) {
+	      			int dx = EYE_RADIUS - w;
+	      			int dy = EYE_RADIUS - h;
+	      			if ((dx*dx + dy*dy) <= (EYE_RADIUS * EYE_RADIUS)) {
+					SDL_SetRenderDrawColor(renderer,254,254,20,255);
+					SDL_RenderDrawPoint(renderer, center.x + dx+37, center.y + dy-140);
+					SDL_RenderDrawPoint(renderer, center.x + dx+143, center.y + dy-140);
+					if((dx*dx + dy*dy) <= ((EYE_RADIUS * EYE_RADIUS)*3)/4){
+						SDL_SetRenderDrawColor(renderer,0,0,0,255);
+						SDL_RenderDrawPoint(renderer, center.x + dx+37, center.y + dy-140);
+						SDL_RenderDrawPoint(renderer, center.x + dx+143, center.y + dy-140);
+					}
+	      			}
+	    		}
+	  	}
 
 	
-}
-
+	}
+private:
+	const int EYE_RADIUS=40;
+	SDL_Renderer* renderer;
+	SDL_Point center;
+	SDL_Color color;
+};
 int main(void){
 	if ( SDL_Init(SDL_INIT_VIDEO) != 0 ) {
     		std::cout << "Failed to initialize SDL2" << std::endl;
@@ -62,7 +82,8 @@ int main(void){
   	SDL_Point center = {250, 250};
   	SDL_Color color = {0,0,0,255};
 	  	
-	drawSaiyin(renderer, center, color);
+	BlackCat blacky(renderer,center,color);
+	blacky.drawSaiyin();
 
   	SDL_RenderPresent(renderer);
 	SDL_Event event;
